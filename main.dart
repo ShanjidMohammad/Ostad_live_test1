eb985fbc617184c3812a8aee0aa38510e3e73dac
ShanjidMohammad <140684880+ShanjidMohammad@users.noreply.github.com>
2024-09-27 23:42:27 +0600

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,26 @@
+class Car {
+
+  String brand;
+  String model;
+  int year;
+
+
+  Car(this.brand, this.model, this.year);
+
+
+  int calculateAge() {
+    int currentYear = DateTime.now().year;
+    return currentYear - year;
+  }
+}
+
+void main() {
+
+  Car car = Car('Toyota', 'Corolla', 2015);
+
+
+  print('Brand: ${car.brand}');
+  print('Model: ${car.model}');
+  print('Year: ${car.year}');
+  print('Car Age: ${car.calculateAge()} years');
+}
